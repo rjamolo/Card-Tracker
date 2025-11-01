cfba073d338d3f4fe3f7514e8aa135460422cbd8
RJ Amolo <royalamolo@gmail.com>
2025-11-01 14:42:20 +0800

    Https changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         Paginator::useBootstrapFive();
     }
 }
